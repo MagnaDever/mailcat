41c165bf648614337d60acf2423ab26382d18690
Aleksandr Simonenko <a.simonenko@innopolis.ru>
2016-11-26 17:41:35 +0300

    page

--- mailer/views.py
+++ mailer/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
- #   response.headers.delete('X-Frame-Options')
-    return render(response.headers.delete('X-Frame-Options'), 'index.html', {})
+    return render(request, 'index.html', {})
